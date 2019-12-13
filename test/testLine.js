8f90547c28a5716484e61e1c98110d95f579ad9a
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2019-12-13 12:56:20 +0530

    Added describe to a Line class in tests.
    What changes you have made?
    Added one more describe function in tests.
    
    Why this change is necessary?
    To identify which functions is of what type while testing.
    
    What is the impact of the change?
    Now, tests will show the Line class upon testing function.

--- test/testLine.js
+++ test/testLine.js
@@ -1,9 +1,11 @@
 const { assert } = require("chai");
 const Line = require("../src/line");
 
-describe("toString", function() {
-  it("should give a string representation of line", function() {
-    const line1 = new Line({ x: 1, y: 2 }, { x: 4, y: 5 });
-    assert.strictEqual(line1.toString(), `Line (1,2),(4,5).`);
+describe("Line", function() {
+  describe("toString", function() {
+    it("should give a string representation of line", function() {
+      const line1 = new Line({ x: 1, y: 2 }, { x: 4, y: 5 });
+      assert.strictEqual(line1.toString(), `Line (1,2),(4,5).`);
+    });
   });
 });
