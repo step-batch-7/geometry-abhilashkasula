941560430ae58ccf7463413338688677d246a813
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2019-12-13 14:03:33 +0530

    Added isEqualTo method to class Line.
    What changes you have made?
    Added function isEqualTo.
    
    Why this change is necessary?
    For checking whether the ends of two lines are same or not.
    
    What is the impact of the change?
    It'll determine whether two lines are same or not.

--- src/line.js
+++ src/line.js
@@ -1,3 +1,9 @@
+const areCoordinatesEqual = (line1, line2) => {
+  const areXCoordinatesEqual = line1.x == line2.x;
+  const areYCoordinatesEqual = line1.y == line2.y;
+  return areXCoordinatesEqual && areYCoordinatesEqual;
+};
+
 class Line {
   constructor(endA, endB) {
     this.endA = { x: endA.x, y: endA.y };
@@ -6,6 +12,13 @@ class Line {
   toString() {
     const { endA, endB } = this;
     return `Line (${endA.x},${endA.y}),(${endB.x},${endB.y}).`;
+  }
+  isEqualTo(line) {
+    const { endA, endB } = line;
+    return (
+      areCoordinatesEqual(this.endA, endA) &&
+      areCoordinatesEqual(this.endB, endB)
+    );
   }
 }
 
--- test/testLine.js
+++ test/testLine.js
@@ -8,4 +8,17 @@ describe("Line", function() {
       assert.strictEqual(line1.toString(), `Line (1,2),(4,5).`);
     });
   });
+
+  describe("isEqualTo", function() {
+    it("should determine true for line ends are same", function() {
+      const line1 = new Line({ x: 1, y: 2 }, { x: 4, y: 5 });
+      const line2 = new Line({ x: 1, y: 2 }, { x: 4, y: 5 });
+      assert.isOk(line1.isEqualTo(line2));
+    });
+    it("should determine false for line ends are not same", function() {
+      const line1 = new Line({ x: 1, y: 2 }, { x: 3, y: 5 });
+      const line2 = new Line({ x: 1, y: 2 }, { x: 4, y: 5 });
+      assert.isNotOk(line1.isEqualTo(line2));
+    });
+  });
 });
