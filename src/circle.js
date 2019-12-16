4976a866b2dce5e4a43310a34ed591d82c0590cf
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2019-12-16 14:58:51 +0530

    Added Circle class and toString method to it

---
+++ src/circle.js
@@ -0,0 +1,12 @@
+class Circle {
+  constructor(centre, radius) {
+    this.centre = { x: centre.x, y: centre.y };
+    this.radius = radius;
+  }
+
+  toString() {
+    return `[Circle @(${this.centre.x},${this.centre.y}) radius ${this.radius}]`;
+  }
+}
+
+module.exports = Circle;
