0ab8ed72d6a7537995eb4a87577f555b9b1f3234
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2019-12-17 17:42:36 +0530

    Added tests for hasPoint to Rectangle

--- test/testRectangle.js
+++ test/testRectangle.js
@@ -78,14 +78,34 @@ describe("Rectangle", () => {
   });
 
   describe("hasPoint", function() {
-    it("should determine true for the point on the left side", function() {
+    it("should determine true for the point on the left line", function() {
       const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
       const point = new Point(5, 4);
+      assert.isTrue(rectangle.hasPoint(point));
+    });
+    it("should determine true for the point on the right line", function() {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      const point = new Point(1, 4);
+      assert.isTrue(rectangle.hasPoint(point));
+    });
+    it("should determine true for the point on the top line", function() {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      const point = new Point(4, 4);
+      assert.isTrue(rectangle.hasPoint(point));
+    });
+    it("should determine true for the point on the bottom line", function() {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      const point = new Point(1, 3);
       assert.isTrue(rectangle.hasPoint(point));
     });
     it("should determine false for the given point is not an instance of Point", function() {
       const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
       const point = { x: 5, y: 4 };
+      assert.isFalse(rectangle.hasPoint(point));
+    });
+    it("should determine false for the given point is not on any of lines of rectangles", function() {
+      const rectangle = new Rectangle({ x: 1, y: 1 }, { x: 5, y: 4 });
+      const point = { x: 2, y: 3 };
       assert.isFalse(rectangle.hasPoint(point));
     });
   });
